41b17adbeef956a4d75f9bcd92732e8a9aa42aa9
anuragchhetri <anurag.chhetri@nagarro.com>
2023-10-05 23:50:58 +0530

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-slate-600'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='bg-slate-900'>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='bg-green-400' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
